5c3c460ef4ff01a7fd438ddbe2eea571a84f3b26
zassory <nicolas.programador@gmail.com>
2023-02-25 23:52:31 -0300

    constructor y funcion tirar dados ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+
+  numero1: number;
+  numero2: number;
+
+  constructor(){
+    this.numero1 = 1;
+    this.numero2 = 2;
+  }
+
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) +1;
+    this.numero2 = Math.round(Math.random() * 5) +1;
+    console.log(this.numero1);
+    console.log(this.numero2);
+  }
+
 }
