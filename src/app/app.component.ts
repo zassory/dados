ad1cd0a2543a465ecf3bc302cfdb4123e9ab19aa
zassory <nicolas.programador@gmail.com>
2023-02-26 17:21:56 -0300

    Proyecto terminado, con ngIf y event binding me comunico con mi ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,12 @@ export class AppComponent {
   tirarDados(): void {
     this.numero1 = Math.round(Math.random() * 5) +1;
     this.numero2 = Math.round(Math.random() * 5) +1;
+    //this.numero1 = Math.round(Math.random() * 5) +1;
+    //this.numero2 = Math.round(Math.random() * 5) +1;
     console.log(this.numero1);
     console.log(this.numero2);
+    //this.dadoIzquierda = `../assets/img/dice${this.numero1}.png`;
+    //this.dadoDerecha = `../assets/img/dice${this.numero2}.png`;
   }
 
 }
